7fe0a7960cf5367a6685243eb1663621b5ba03d5
rkengoum <rodrigue.kengoum-gassam@capgemini.com>
2024-05-01 03:28:53 +0200

    add element in constant

--- src/main/java/com/example/demo/stl_figure/Constant.java
+++ src/main/java/com/example/demo/stl_figure/Constant.java
@@ -11,6 +11,17 @@ public class Constant {
     public static String normal = "facet normal ";
     public static String space = " ";
     public static String empty = "";
+    public static String invalidColor = "Invalid color code, exemple .123;.23;.34 or .123;.34;.34;0.2";
+    public static String comma = ";";
+    public static String invalidColorComponent = "Invalid color component. one of your color component are not double";
+    public static String enterIp = "Enter IP:";
+    public static String selectOption = "Select Option:";
+    public static String validateButton = "Validate";
+    public static String exitConfirmation = "Exit Confirmation";
+    public static String wantToExit = "Are you sure you want to exit?";
+    public static String choose = "Click OK to exit or Cancel to stay.";
+    public static String dialogTitle = "Connect Hub";
+    public static String appTitle = "STL visualizer";
 
     public static String lineXtoX(int i) {
         return "Invalid vertex format. Look between line " + (i + 1) + " and " + (i + 3);
