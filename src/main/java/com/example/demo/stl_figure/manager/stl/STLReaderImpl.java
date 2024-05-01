3e8df2ec37d6211a7d6132f56a89437b9104ddfe
rkengoum <rodrigue.kengoum-gassam@capgemini.com>
2024-05-01 03:07:15 +0200

    add Constant

--- src/main/java/com/example/demo/stl_figure/manager/stl/STLReaderImpl.java
+++ src/main/java/com/example/demo/stl_figure/manager/stl/STLReaderImpl.java
@@ -1,6 +1,7 @@
 package com.example.demo.stl_figure.manager.stl;
 
 import com.example.demo.HelloApplication;
+import com.example.demo.stl_figure.Constant;
 import com.example.demo.stl_figure.exception.Exception;
 import com.example.demo.stl_figure.model.Facet;
 import com.example.demo.stl_figure.model.Polygon;
@@ -12,22 +13,18 @@ import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class STLReaderImpl implements STLReader {
-    @Override
+public class STLReaderImpl {
     public Polyhedron readSTLFile(String fileName) throws URISyntaxException {
         File file = new File(HelloApplication.class.getResource("data/" + fileName).toURI());
 
         try (InputStream inputStream = new FileInputStream(file)) {
             if (false) {
                 // Read binary STL file
-                // Implement binary STL file parsing
-                throw new UnsupportedOperationException("Binary STL file parsing is not implemented yet.");
+                throw new UnsupportedOperationException(Constant.binaryReadImplementation);
             } else {
                 // Read ASCII STL file
                 return readAsciiSTL(inputStream);
             }
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -54,18 +51,18 @@ public class STLReaderImpl implements STLReader {
         while(i < fileLines.size()) {
             String line = fileLines.get(i);
             if(i == 0) {
-                if(!line.startsWith("solid "))
-                    Exception.raiseException("Invalid solid format. line 1");
+                if(!line.startsWith(Constant.solid))
+                    Exception.raiseException(Constant.line1);
 
-                solidName = line.trim().replace("solid ", "");
+                solidName = line.trim().replace(Constant.solid, Constant.empty);
                 i += 1;
             }else if (i == fileLines.size() - 1) {
                 break;
             }else {
-                if(!line.startsWith("facet normal ")) {
-                    Exception.raiseException("Invalid facet format. line " + i);
+                if(!line.startsWith(Constant.normal)) {
+                    Exception.raiseException(Constant.line + i);
                 } else {
-                    String[] coords = line.replace("facet normal ", "").split(" ");
+                    String[] coords = line.replace(Constant.normal, Constant.empty).split(Constant.space);
 
                     Top normal = new Top(
                             toDouble(coords[0]),
@@ -91,7 +88,6 @@ public class STLReaderImpl implements STLReader {
                     polygons.add(p3);
 
                     Facet facet = new Facet(normal, vertexs, polygons);
-                    Sync.calculateSurface(facet);
                     facets.add(facet);
 
                     i += 7;
@@ -104,10 +100,10 @@ public class STLReaderImpl implements STLReader {
 
     private List<Top> productTop(int i, String vertex1, String vertex2, String vertex3) {
         Top v1 = null, v2 = null, v3 = null;
-        if(vertex1.startsWith("vertex ") && vertex2.startsWith("vertex ") && vertex3.startsWith("vertex ")) {
-            String[] ver1 = vertex1.replace("vertex ", "").split(" ");
-            String[] ver2 = vertex2.replace("vertex ", "").split(" ");
-            String[] ver3 = vertex3.replace("vertex ", "").split(" ");
+        if(vertex1.startsWith(Constant.vertex) && vertex2.startsWith(Constant.vertex) && vertex3.startsWith(Constant.vertex)) {
+            String[] ver1 = vertex1.replace(Constant.vertex, Constant.empty).split(Constant.space);
+            String[] ver2 = vertex2.replace(Constant.vertex, Constant.empty).split(Constant.space);
+            String[] ver3 = vertex3.replace(Constant.vertex, Constant.empty).split(Constant.space);
             v1 = new Top(
                     toDouble(ver1[0]),
                     toDouble(ver1[1]),
@@ -124,8 +120,7 @@ public class STLReaderImpl implements STLReader {
                     toDouble(ver3[2])
             );
         } else {
-            Exception.raiseException("Invalid vertex format " +
-                    ". Look between line " + (i + 1) + " and " + (i + 3));
+            Exception.raiseException(Constant.lineXtoX(i));
         }
         List<Top> array = new ArrayList<>();
         array.add(v1);
@@ -138,7 +133,7 @@ public class STLReaderImpl implements STLReader {
         try {
             return Double.parseDouble(value);
         } catch(java.lang.Exception e) {
-            Exception.raiseException("Some value in you file are not double value");
+            Exception.raiseException(Constant.notDouble);
         }
         return 0.0;
     }
